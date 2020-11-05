3289aa9fab495bb4f501cac3e1f444f8fdb2b5a0
Vinod Vyankatesh Gentyal <Vinod.gentyal@gmail.com>
2020-11-05 09:28:32 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,9 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <></>
+    <div data-ns-test="project-name">Gaming Platform</div>
+    <div data-ns-test="project-description">In this App the users can play games like chess,Tic Tac Toe,Math clash in single player and in multiplayer also</br>
+    in additional features they can chat online and see who is in leaderboard etc.</div>
   )
 }
 

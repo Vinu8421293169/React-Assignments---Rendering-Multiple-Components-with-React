a5a2b4873a64ef3e054303872ed94f0ed8dc58da
Vinod Vyankatesh Gentyal <Vinod.gentyal@gmail.com>
2020-11-05 09:44:10 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div data-ns-test="project-name">Gaming Platform</div>
-    <div data-ns-test="project-description">In this App the users can play games like chess, Tic Tac Toe, Ludo and Math clash in single player and in multiplayer also</br>
-    in additional features they can chat online and see who is in leaderboard etc.</div>
-  )
-}
+    <>
+      <div data-ns-test="project-name">Gaming Platform</div>
 
+      <div data-ns-test="project-description">
+        In this App the users can play games like chess, Tic Tac Toe, Ludo and
+        Math clash in single player and in multiplayer also in additional
+        features they can chat online and see who is in leaderboard etc.
+      </div>
+    </>
+  );
+};
 
 export default App;
